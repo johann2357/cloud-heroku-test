eaa20e5c8a09b5150ef45e6d8bbaa628e46a533e
johann2357 <johann2357@gmail.com>
2016-09-16 20:05:06 -0500

    test var

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,5 @@
-import requests
+# import requests
+import os
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -8,10 +9,12 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
+    times = int(os.environ.get('TIMES', 3))
+    return HttpResponse('Hello! ' * times)
+    # r = requests.get('http://httpbin.org/status/418')
     # print r.text
 
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    # return HttpResponse('<pre>' + r.text + '</pre>')
     # return HttpResponse('Hello from Python!')
     # return render(request, 'index.html')
 
